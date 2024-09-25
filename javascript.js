1723fd5e8566e22ca5753f9e0dfb69e133ae3d79
Pesquisa-TCC-Marcas <87100062+Pesquisa-TCC-Marcas@users.noreply.github.com>
2024-09-25 20:18:42 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,12 +2,12 @@ function sortearLink() {
     var numeroSorteado = Math.floor(Math.random() * 6) + 1;
 
     var links = {
-        1: "",
-        2: "",
-        3: "",
-        4: "",
-        5: "",
-        6: ""
+        1: "https://forms.gle/8F5UeALjZUwQ8mvw9",
+        2: "https://forms.gle/sdiXHg94XghuMCP18",
+        3: "https://forms.gle/gXJf7gnQZLGEmEkNA",
+        4: "https://forms.gle/TztUXrYmQ8q1VtfbA",
+        5: "https://forms.gle/HU16G3mDy7X9TYV16",
+        6: "https://forms.gle/BGUsdVJpEbhmmfSe8"
     };
 
     window.location.href = links[numeroSorteado];
